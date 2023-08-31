93f9e9c30118142d2d77ee72c506d5174b85d766
nastiadev <agorodinskaya@live.com>
2023-08-31 13:26:26 +1000

    some more stuff done

--- src/components/layouts/MainNavigation.js
+++ src/components/layouts/MainNavigation.js
@@ -1,22 +1,26 @@
 import { Link } from 'react-router-dom';
+import classes from './MainNavigation.module.css';
 
 function MainNavigation() {
   return (
     <header>
-      <div>React meetups</div>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>All Meetups</Link>
-          </li>
-          <li>
-            <Link to='/new-meetup'>Add new meetup</Link>
-          </li>
-          <li>
-            <Link to='/favourites'>My favourites</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className={classes.header}>
+        <div className={classes.logo}>React meetups</div>
+
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>All Meetups</Link>
+            </li>
+            <li>
+              <Link to='/new-meetup'>Add new meetup</Link>
+            </li>
+            <li>
+              <Link to='/favourites'>My favourites</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,32 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_DATA = [
+  {
+    id: 'm1',
+    title: 'This is a first meetup',
+    image:
+      'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+  {
+    id: 'm2',
+    title: 'This is a second meetup',
+    image:
+      'https://images.unsplash.com/photo-1506973035872-a4ec16b8e8d9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80',
+    address: 'Meetupstreet 5, 12345 Meetup City',
+    description:
+      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
+  },
+];
 function AllMeetupsPage(params) {
-  return <div>All Meet ups</div>;
+  return (
+    <div>
+      <h1>All Meet ups</h1>
+      <MeetupList meetups={DUMMY_DATA} />
+    </div>
+  );
 }
 
 export default AllMeetupsPage;
